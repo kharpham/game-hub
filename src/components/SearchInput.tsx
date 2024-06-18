786a6d36c347c888b11111dc0d05a623e6acfcfd
Khapham <khar22102003@gmail.com>
2024-06-18 11:15:01 +0700

    Fix navbar

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,15 +2,18 @@ import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 import useGameQueryStore from "../store";
+import { useNavigate } from "react-router-dom";
 const SearchInput = () => {
   const setSearch = useGameQueryStore((s) => s.setSearch);
   const searchRef = useRef<HTMLInputElement>(null);
+  const navigate = useNavigate();
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
         if (searchRef.current) {
           setSearch(searchRef.current.value);
+          navigate("/");
         }
       }}
     >
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading, Spinner} from '@chakra-ui/react'
+import { Box, GridItem, Heading, SimpleGrid, Spinner} from '@chakra-ui/react'
 import useDetails from '../hooks/useDetails'
 import { useParams } from 'react-router-dom';
 import ExpandableText from '../components/ExpandableText';
@@ -13,15 +13,19 @@ const GameDetailPage = () => {
   if (isLoading) return <Spinner/>;
   if (error || !game) throw Error;
   return (
-    <Box>
+    <SimpleGrid columns={{base: 1, md: 2}} spacing={5}>
+      <GridItem>
       <Heading>{game?.name}</Heading>
       <ExpandableText>
         {game?.description_raw || ""}
       </ExpandableText>
       <GameAttributes game={game}></GameAttributes>
-      <GameTrailer gameId={game.id}/>
-      <GameScreenshot gameId={game.id}/>
-    </Box> 
+      </GridItem>
+      <GridItem>
+        <GameTrailer gameId={game.id}/>
+        <GameScreenshot gameId={game.id}/>
+      </GridItem>
+    </SimpleGrid> 
   )
 }
 
